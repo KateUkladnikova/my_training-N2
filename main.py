61b6e74d252f791f9a6aff168f13334201e2c87c
KateUkladnikova <ekaterina.ukladnikova2@gmail.com>
2024-09-24 01:43:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# "1st program"
+print(9**0.5*5)
+# "2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+# "3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+# "4th program"
+str_a = '123.456'
+c = float(str_a)*10.0
+e = int(c)
+print(e%10)
