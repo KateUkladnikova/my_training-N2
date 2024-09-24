e178057384c1b3883d1343a4a6c165ed4cd8cb3a
KateUkladnikova <ekaterina.ukladnikova2@gmail.com>
2024-09-24 21:45:57 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Динамическая типизация"
+name = "Екатерина"
+print("Name: " + name)
+age = 55
+print("Age: ", age)
+age = age + 1
+print("New Age:", age)
+is_student = True
+print("Is Student: ", is_student)
