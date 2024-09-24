144419039bd0b4c93b028e9942ce9618937d16c1
KateUkladnikova <ekaterina.ukladnikova2@gmail.com>
2024-09-24 23:47:27 +0300

    Add files via upload
    Практическое задание по уроку "Строки и индексация строк"

---
+++ Lesson_Strings.py
@@ -0,0 +1,7 @@
+# Практическое задание по уроку "Строки и индексация строк".
+example = 'erunda_eto'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:10:2])
