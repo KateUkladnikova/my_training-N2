2ac31dd8659b5cad02af61040aef26b488437fa2
KateUkladnikova <ekaterina.ukladnikova2@gmail.com>
2024-09-25 23:50:14 +0300

    Add files via upload
    Практическая работа по уроку "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+# Практическая работа по уроку "Организация программ и методы строк"
+my_string = input("Напишите, пожалуйста, что-нибудь: " )
+# Вывoд количества символов введённого текста:
+print(len(my_string))
+# Вывoд строки my_string в верхнем регистре:
+print(my_string.upper())
+# Вывoд строки my_string в нижнем регистре:
+print(my_string.lower())
+# Изменяю строку my_string, удалив все пробелы:
+print(my_string.replace(" ",""))
+# Вывожу первый символ строки my_string:
+print(my_string[0])
+# Вывожу последний символ строки my_string:
+print(my_string[-1])
